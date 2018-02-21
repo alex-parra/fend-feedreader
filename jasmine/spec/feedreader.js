894d6991650b57acb6faba3f8b0f6b22afe02c86
Alex Parra <alexpds@gmail.com>
2018-02-21 22:06:06 +0000

    Added tests for allFeeds list

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,7 @@ $(function() {
      * allFeeds in app.js to be an empty array and refresh the
      * page?
      */
-    it('are defined', function() {
+    it('Feeds list is defined and not empty', function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
@@ -31,22 +31,35 @@ $(function() {
      * in the allFeeds object and ensures it has a URL defined
      * and that the URL is not empty.
      */
+    allFeeds.forEach(function(f, i) {
+      it('Feed at index '+ i +' has url property and is not empty', function() {
+        expect(f.url).toBeDefined();
+        expect(f.url).toBeTruthy();
+      });
+    });
 
 
     /* TODO: Write a test that loops through each feed
      * in the allFeeds object and ensures it has a name defined
      * and that the name is not empty.
      */
+    allFeeds.forEach(function(f, i) {
+      it('Feed at index '+ i +' has name property and is not empty', function() {
+        expect(f.name).toBeDefined();
+        expect(f.name).toBeTruthy();
+      });
+    });
   });
 
 
   /* TODO: Write a new test suite named "The menu" */
-
+  describe('The menu', function() {
     /* TODO: Write a test that ensures the menu element is
      * hidden by default. You'll have to analyze the HTML and
      * the CSS to determine how we're performing the
      * hiding/showing of the menu element.
      */
+
 
      /* TODO: Write a test that ensures the menu changes
       * visibility when the menu icon is clicked. This test
@@ -54,19 +67,25 @@ $(function() {
       * clicked and does it hide when clicked again.
       */
 
-  /* TODO: Write a new test suite named "Initial Entries" */
 
+  });
+
+  /* TODO: Write a new test suite named "Initial Entries" */
+  describe('Initial Entries', function() {
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      * Remember, loadFeed() is asynchronous so this test will require
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
+  });
 
   /* TODO: Write a new test suite named "New Feed Selection" */
-
+  describe('New Feed Selection', function() {
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
+  });
+
 }());
--- js/app.js
+++ js/app.js
@@ -40,48 +40,43 @@ function init() {
  * This function all supports a callback as the second parameter
  * which will be called after everything has run successfully.
  */
- function loadFeed(id, cb) {
-   var feedUrl = allFeeds[id].url,
-     feedName = allFeeds[id].name;
-
-   $.ajax({
-     type: "POST",
-     url: 'https://rsstojson.udacity.com/parseFeed',
-     data: JSON.stringify({url: feedUrl}),
-     contentType:"application/json",
-     success: function (result, status){
-
-         var container = $('.feed'),
-           title = $('.header-title'),
-           entries = result.feed.entries,
-           entriesLen = entries.length,
-           entryTemplate = Handlebars.compile($('.tpl-entry').html());
-
-         title.html(feedName);   // Set the header text
-         container.empty();      // Empty out all previous entries
-
-         /* Loop through the entries we just loaded via the Google
-          * Feed Reader API. We'll then parse that entry against the
-          * entryTemplate (created above using Handlebars) and append
-          * the resulting HTML to the list of entries on the page.
-          */
-         entries.forEach(function(entry) {
-           container.append(entryTemplate(entry));
-         });
-
-         if (cb) {
-           cb();
-         }
-         },
-     error: function (result, status, err){
-         //run only the callback without attempting to parse result due to error
-         if (cb) {
-           cb();
-         }
-         },
-     dataType: "json"
-   });
- }
+function loadFeed(id, cb) {
+  var feedUrl = allFeeds[id].url,
+    feedName = allFeeds[id].name;
+  $.ajax({
+    type: "POST",
+    url: 'https://rsstojson.udacity.com/parseFeed',
+    data: JSON.stringify({url: feedUrl}),
+    contentType:"application/json",
+    success: function (result, status){
+      var container = $('.feed'),
+          title = $('.header-title'),
+          entries = result.feed.entries,
+          entriesLen = entries.length,
+          entryTemplate = Handlebars.compile($('.tpl-entry').html());
+      title.html(feedName);   // Set the header text
+      container.empty();      // Empty out all previous entries
+      /* Loop through the entries we just loaded via the Google
+        * Feed Reader API. We'll then parse that entry against the
+        * entryTemplate (created above using Handlebars) and append
+        * the resulting HTML to the list of entries on the page.
+        */
+      entries.forEach(function(entry) {
+        container.append(entryTemplate(entry));
+      });
+      if (cb) {
+        cb();
+      }
+    },
+    error: function (result, status, err){
+      //run only the callback without attempting to parse result due to error
+      if (cb) {
+        cb();
+      }
+    },
+    dataType: "json"
+  });
+}
 
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
@@ -96,7 +91,6 @@ $(function() {
   var container = $('.feed'),
     feedList = $('.feed-list'),
     feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
-    feedId = 0,
     menuIcon = $('.menu-icon-link');
 
   /* Loop through all of our feeds, assigning an id property to
@@ -105,11 +99,9 @@ $(function() {
    * above using Handlebars) and append it to the list of all
    * available feeds within the menu.
    */
-  allFeeds.forEach(function(feed) {
+  allFeeds.forEach(function(feed, feedId) {
     feed.id = feedId;
     feedList.append(feedItemTemplate(feed));
-
-    feedId++;
   });
 
   /* When a link in our feedList is clicked on, we want to hide
@@ -130,4 +122,5 @@ $(function() {
   menuIcon.on('click', function() {
     $('body').toggleClass('menu-hidden');
   });
+
 }());
