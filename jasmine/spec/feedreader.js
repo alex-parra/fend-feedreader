3e1573d919bfa5c7b44512208ad9cd30b1127475
Alex Parra <alexpds@gmail.com>
2018-02-22 23:29:31 +0000

    Improved jasmine tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,113 +9,106 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-  /* This is our first test suite - a test suite just contains
-  * a related set of tests. This suite is all about the RSS
-  * feeds definitions, the allFeeds variable in our application.
-  */
+
+  /*
+   * Tests to the RSS Feeds 'allFeeds' variable
+   */
   describe('RSS Feeds', function() {
-    /* This is our first test - it tests to make sure that the
-     * allFeeds variable has been defined and that it is not
-     * empty. Experiment with this before you get started on
-     * the rest of this project. What happens when you change
-     * allFeeds in app.js to be an empty array and refresh the
-     * page?
-     */
+
     it('> Feeds list is defined and not empty', function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
 
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a URL defined
-     * and that the URL is not empty.
-     */
+    // Test that every feed has a url property, it is a string and it is not empty
     allFeeds.forEach(function(f, i) {
       it('> Feed at index '+ i +' has url property and is not empty', function() {
-        expect(f.url).toBeDefined();
-        expect(f.url.length).not.toBe(0);
+        expect(f.url).toBeDefined(); // ensure it is set
+        expect(f.url).toEqual(jasmine.any(String)); // ensure it's a string
+        expect(f.url).not.toBe(''); // ensure it's not an empty string
       });
     });
 
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a name defined
-     * and that the name is not empty.
-     */
+    // Test that every feed has a name property, it is a string and it is not empty
     allFeeds.forEach(function(f, i) {
       it('> Feed at index '+ i +' has name property and is not empty', function() {
-        expect(f.name).toBeDefined();
-        expect(f.name.length).not.toBe(0);
+        expect(f.name).toBeDefined(); // ensure it is set
+        expect(f.name).toEqual(jasmine.any(String)); // ensure it's a string
+        expect(f.name).not.toBe(''); // ensure it's not an empty string
       });
     });
-  });
+
+  }); // RSS Feeds suite
 
 
-  /* TODO: Write a new test suite named "The menu" */
+
+  /*
+   * Tests to the Menu behavior
+   */
   describe('The menu', function() {
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
+
+    // Test that the body has expected class on load
     it('> Menu is hidden by default: body class', function() {
       expect($('body').hasClass('menu-hidden')).toBe(true);
     });
 
+    // Test that the menu is in fact offscreen
     it('> Menu is hidden by default: menu is Off-Screen', function() {
       // tests if left edge of menu is off to the left of the screen (negative offset)
       expect($('.slide-menu').offset().left).toBeLessThan(0);
     });
 
-
-     /* TODO: Write a test that ensures the menu changes
-      * visibility when the menu icon is clicked. This test
-      * should have two expectations: does the menu display when
-      * clicked and does it hide when clicked again.
-      */
+     // Test that the menu responds correctly to clicks
     describe('> Menu changes visibility on click', function(){
+
+      // Before each test, toggle the menu by simulating a click
       beforeEach(function(done){
         $('.menu-icon-link').trigger('click');
-        setTimeout(() => done(), 250);
+        setTimeout(() => done(), 250); // we need to wait a bit while the animation runs
       });
 
+      // Test that the menu, when closed, opens:
+      // > body does not have related css class and menu is in fact visible (css testing)
       it('> Test open menu: body class + is On-Screen', function() {
         expect($('body').hasClass('menu-hidden')).toBe(false);
         expect($('.slide-menu').offset().left).toBe(0);
       });
 
+      // Test that the menu, when open, closes:
+      // > body has css class (js testing) and menu is in fact off screen (css testing)
       it('> Test close menu: body class + is Off-Screen', function() {
         expect($('body').hasClass('menu-hidden')).toBe(true);
         expect($('.slide-menu').offset().left).toBeLessThan(0);
       });
     });
-  });
 
-  /* TODO: Write a new test suite named "Initial Entries" */
+  }); // The menu suite
+
+
+
+  /*
+   * Tests to the initial feed items
+   */
   describe('Initial Entries', function() {
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+
+    // Ensure the feed is loaded before running tests
     beforeEach(function(done){
       loadFeed(0, done);
     });
 
     it('> At least 1 entry was loaded and added to the DOM', function() {
       var entries = $('.feed .entry');
-      expect(entries.length).not.toBeLessThan(1);
+      expect(entries.length).toBeGreaterThan(0);
     });
-  });
 
-  /* TODO: Write a new test suite named "New Feed Selection" */
+  }); // Initial entries suite
+
+
+
+  /*
+   * Tests to changing Feeds
+   */
   describe('New Feed Selection', function() {
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
     var firstFeedEntries = [];
 
     beforeAll(function(done){
@@ -138,6 +131,8 @@ $(function() {
       expect(entries.eq(0).attr('href')).not.toBe(firstFeedEntries.eq(0).attr('href'));
     });
 
-  });
+  }); // Changing feeds suite
+
+
 
 }());
